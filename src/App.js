fd387604ee49e101809582ffbe15afb8b7f3e393
ebisLab <ebaiden@techie.com>
2020-02-20 16:57:13 -0500

    provider wrapping app

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, createContext } from 'react';
 import { Route } from 'react-router-dom';
 import data from './data';
 
@@ -7,19 +7,21 @@ import Navigation from './components/Navigation';
 import Products from './components/Products';
 import ShoppingCart from './components/ShoppingCart';
 
+
+const ProductContext =createContext()
 function App() {
 	const [products] = useState(data);
 	const [cart, setCart] = useState([]);
 
-	const addItem = (item, e) => {
+	const addItem = (item) => {
 		// add the given item to the cart
 		console.log('Cliecked on the add Item')
 		console.log(item)
 		setCart([...cart, item])
 	};
-console.log(products, 'products')
 	return (
 		<div className="App">
+			<ProductContext.Provider value={cart}>
 			<Navigation cart={cart} />
 
 			{/* Routes */}
@@ -38,6 +40,7 @@ console.log(products, 'products')
 				path="/cart"
 				render={() => <ShoppingCart cart={cart} />}
 			/>
+			</ProductContext.Provider>
 		</div>
 	);
 }

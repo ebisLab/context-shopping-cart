b615be53d3d497e7bf6b316ae8b5a749226914ab
ebisLab <ebaiden@techie.com>
2020-02-20 16:59:39 -0500

    added products and additem as value props

--- src/App.js
+++ src/App.js
@@ -21,7 +21,7 @@ function App() {
 	};
 	return (
 		<div className="App">
-			<ProductContext.Provider value={cart}>
+			<ProductContext.Provider value={{products, addItem}}>
 			<Navigation cart={cart} />
 
 			{/* Routes */}
